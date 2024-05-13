455619266f401ea85ce0d4598740cc124f7772a2
bartek8550 <bkbart7@gmail.com>
2024-05-13 22:15:31 +0200

    Update Added System.out

--- src/main/java/wit/proj/Main.java
+++ src/main/java/wit/proj/Main.java
@@ -14,6 +14,7 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("i = " + i*2);
             System.out.println("i = " + i*5);
+            System.out.println("i = " + i*7);
         }
 
     }
