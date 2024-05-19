b2ea4c7a59254edee5181b707310f533fc0f19ab
Szyrandol <szymomaster@gmail.com>
2024-05-19 17:38:35 +0200

    had to commit main either way

--- src/main/java/wit/proj/Main.java
+++ src/main/java/wit/proj/Main.java
@@ -1,13 +1,10 @@
 package wit.proj;
 
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         // złego miłe sa początki
-        //jakaś kolejka tych plików do wczytania
-        //potem lista z kolejki
 
 
         for (int i = 1; i <= 5; i++) {
