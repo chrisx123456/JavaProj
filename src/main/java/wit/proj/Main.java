44a9d0d575aae16f7f4678238c01ece1cb00c8a3
Trakol12 <alsmok3@gmail.com>
2024-05-19 19:00:32 +0200

    Added comment for javadoc generation

--- src/main/java/wit/proj/Main.java
+++ src/main/java/wit/proj/Main.java
@@ -6,27 +6,14 @@ import java.io.File;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!!! TO ja Trakol112" );
-
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i+234);
-            System.out.println("i = " + i+11);
-            System.out.println("i = " + i+1112);
-
-        }
         String sourcePath = "C:\\Users\\alsmo\\Desktop\\obrazy do testowania projektu z javy";
         String destinationPath = "C:\\Users\\alsmo\\Desktop\\obrazy do wklejenia";
         FileManager filemanager = new FileManager(sourcePath,destinationPath);
         try {
-            FileManager.getImagePaths(new File(sourcePath));
-            System.out.println(filemanager.getImageList());
+            FileManager.getImagePaths(new File(FileManager.getSourcePath()));
+            //System.out.println(filemanager.getImageList());
             System.out.println("Pobrano ścieżki");
-            FileManager.Save(new File(destinationPath),filemanager.getImageList());
+            FileManager.Save(new File(destinationPath));
             System.out.println("Przekopiowano");
 
         } catch (Exception e) {
