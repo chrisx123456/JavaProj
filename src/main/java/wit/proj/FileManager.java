cd4e8bcff6e4848bc1a1458d83c624dad14ed76e
Trakol12 <alsmok3@gmail.com>
2024-05-20 17:23:34 +0200

    Final verison without threds

--- src/main/java/wit/proj/FileManager.java
+++ src/main/java/wit/proj/FileManager.java
@@ -14,10 +14,9 @@ import java.util.List;
  * @version 1.0
  */
 public class FileManager  {
-    /**Lista obiektów klasy Image przechowywująca ścieżki do pliku oraz datę utworzenia */
-     public static List<Image> imageList = new ArrayList<>();
-    /**Zmienne String przechowywujące ścieżki do folderu początkowego oraz wynikowego*/
-     public static String sourcePath,destinationPath;
+
+     public static List<Image> imageList = new ArrayList<>(); //Lista obiektów klasy Image przechowywująca ścieżki do pliku oraz datę utworzenia
+     public static String sourcePath,destinationPath; //Zmienne String przechowywujące ścieżki do folderu początkowego oraz wynikowego
 
 
     /**
@@ -40,26 +39,21 @@ public class FileManager  {
      */
     public static void getImagePaths(File folder) throws Exception
     {
-        /**Pobranie listy plików w folderze docelowym*/
-        File[] files = folder.listFiles();
-        /**Jeśli folder jest pusty przerywa działanie*/
-        if(files == null){
+        File[] files = folder.listFiles();//Pobranie listy plików w folderze docelowym
+        if(files == null){//Jeśli folder jest pusty przerywa działanie/
             System.out.println("No files found");
             return;
         }
-        /**Przechodzi przez wszystkie pliki w folderze po koleji*/
-        for (File file : files) {
-            /**Jeśli dany plik jest folderem to wywołuję tą metodę ponownie z nową ścieżką*/
-            if(file.isDirectory()){
+
+        for (File file : files) {//Przechodzi przez wszystkie pliki w folderze po koleji
+            if(file.isDirectory()){ //Jeśli dany plik jest folderem to wywołuję tą metodę ponownie z nową ścieżką
                 getImagePaths(file);
             }
-            /**Jeśli dany plik jest folderem i jest typu jpg*/
-            else if(file.isFile() && file.getName().endsWith(".jpg")){
+            else if(file.isFile() && file.getName().endsWith(".jpg")){//Jeśli dany plik jest folderem i jest typu jpg
                 try{
                     Image image = new Image(file.getAbsolutePath());
                     String date = image.getCreationDate();
-                    /**Jeśli dany plik nie ma daty EXIF to nie dodaje go do listy*/
-                    if(date == null)
+                    if(date == null) //Jeśli dany plik nie ma daty EXIF to nie dodaje go do listy
                     {
                         System.out.println("Missing date: "+ file.getAbsolutePath());
                     }
@@ -83,36 +77,29 @@ public class FileManager  {
      */
     public static void Save (File folder) throws Exception{
        // ChceckIfDirectoryExists();
-        /**Jeśli lista obiektów jest pusta to przerywa działanie*/
-        if(imageList == null){
+        if(imageList == null){        //Jeśli lista obiektów jest pusta to przerywa działanie
             System.out.println("No correct files found to copy ");
             return;
         }
         int HowManyHaveBeenCopied = 0;
-        /**Przechodzi przez każdy obiekt w liście*/
-        for(Image image : imageList){
+        for(Image image : imageList){//Przechodzi przez każdy obiekt w liście
             String date = image.getCreationDate();
             File subFolder = new File(folder.getAbsolutePath(), date);
 
-            /**Jeśli folder nie istnieje i nie jest folderem*/
-            if (!subFolder.exists() && !subFolder.isDirectory()) {
-                     /**Próbuje utworzyć foldery.Jeśli się nie uda wyrzuca błąd*/
-                    if(!subFolder.mkdirs()){
+            if (!subFolder.exists() || !subFolder.isDirectory()) { //Jeśli folder nie istnieje i nie jest folderem
+                    if(!subFolder.mkdirs()){//Próbuje utworzyć foldery.Jeśli się nie uda wyrzuca błąd
                         throw new Exception("Failed to create folder " + subFolder.getAbsolutePath());
                     }
             }
 
             //Kopiowanie pliku do nowego foldru
             long numberOfFilesInFolder = 0;
-            /**Jeśli folder nie jest pusty przypisuje liczbę plików w nim do zmiennej*/
-            if(subFolder.listFiles()!= null){
+            if(subFolder.listFiles()!= null){//Jeśli folder nie jest pusty przypisuje liczbę plików w nim do zmiennej
                 numberOfFilesInFolder = subFolder.listFiles().length;
             }
-            /**Tworzenie nowej ścieżki dla pliku*/
-            Path imagePathWithName = Paths.get(subFolder.getAbsolutePath()).resolve(numberOfFilesInFolder + 1 +".jpg");
+            Path imagePathWithName = Paths.get(subFolder.getAbsolutePath()).resolve(numberOfFilesInFolder + 1 +".jpg"); //Tworzenie nowej ścieżki dla pliku
             try {
-                /**Kopjuje plik*/
-                Files.copy(Paths.get(image.getSourcePath()), imagePathWithName);
+                Files.copy(Paths.get(image.getSourcePath()), imagePathWithName); //Kopjuje plik
                 HowManyHaveBeenCopied++;
             } catch (IOException e) {
                 throw new RuntimeException("Failed to Copy file", e);
