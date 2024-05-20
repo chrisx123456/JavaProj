314863b1ad188cc3dbdfffca139a488e29be3499
bartek8550 <bkbart7@gmail.com>
2024-05-20 22:04:57 +0200

    Update Added another tests

--- src/test/java/wit/proj/FileManagerTest.java
+++ src/test/java/wit/proj/FileManagerTest.java
@@ -3,15 +3,19 @@ package wit.proj;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class FileManagerTest {
 
     @Test
-    void getImagesPathsTest() throws Exception {
+    void getImagesPathsTest() {
         //Arrange
         FileManager fm = new FileManager();
         File testFolder = new File("src/test/resources/testowy");
@@ -21,7 +25,7 @@ class FileManagerTest {
 
         //Assert
         assertNotNull(fm.pathsList);
-        assertTrue(fm.pathsList.size() == 57);
+        assertTrue(fm.pathsList.size() >= 57);
 
     }
 
@@ -64,7 +68,7 @@ class FileManagerTest {
 
     @Test
     void createImageListEmptyListTest() throws Exception{
-        //Przypadek gdzie lista plików zawiera pliki bez daty wykonania
+        //Przypadek gdzie lista plików jest pusta
         //Arrange
         List<File> empty = new ArrayList<>();
 
@@ -72,12 +76,89 @@ class FileManagerTest {
         Exception exception = assertThrows(Exception.class, () -> FileManager.CreateImageList(empty));
 
         //Assert
-        assertEquals("No images at given dir", exception.getMessage());
+        assertEquals("Paths list is null or empty", exception.getMessage());
 
     }
 
     @Test
-    void save() {
+    void createImageListNullListTest() throws Exception{
+        //Przypadek gdzie lista plików jest nullem
+        //Arrange & Act
+        Exception exception = assertThrows(Exception.class, () -> FileManager.CreateImageList(null));
 
+        //Assert
+        assertEquals("Paths list is null or empty", exception.getMessage());
+
+    }
+
+    @Test
+    void saveTest() throws Exception{
+        //Arrange
+        File folder = new File("src/test/resources/testowy");
+        Image img1 = new Image("src/test/resources/testowy/Archer.(Fate.stay.night).full.1801264.jpg");
+        Image img2 = new Image("src/test/resources/testowy/caster-fate-grand-order-solomon-goetia-anime-34552-resized.jpg");
+        List<Image> images = Arrays.asList(img1, img2);
+
+        //Act
+        FileManager.Save(folder, images);
+
+        //Assert
+        File subFolder1 = new File(folder, img1.getCreationDate());
+        File subFolder2 = new File(folder, img2.getCreationDate());
+        //
+        assertTrue(subFolder1.exists());
+        assertTrue(subFolder1.isDirectory());
+        assertTrue(subFolder1.listFiles().length >= 1);
+        //
+        assertTrue(subFolder2.exists());
+        assertTrue(subFolder2.isDirectory());
+        assertTrue(subFolder2.listFiles().length >= 1);
+    }
+
+    @Test
+    void saveNullPointerExceptionForNullListTest() throws Exception{
+        //Arrange
+        File folder = new File("src/test/resources/testowy");
+        List<Image> images = null;
+
+        //Act & Assert
+        Exception exception = assertThrows(NullPointerException.class, () -> FileManager.Save(folder, images));
+        assertEquals("Image list is null or empty", exception.getMessage());
+    }
+
+    @Test
+    void saveNullPointerExceptionForEmptyListTest() throws Exception{
+        //Arrange
+        File folder = new File("src/test/resources/testowy");
+        List<Image> images = Collections.emptyList();
+
+        //Act & Assert
+        Exception exception = assertThrows(NullPointerException.class, () -> FileManager.Save(folder, images));
+        assertEquals("Image list is null or empty", exception.getMessage());
+    }
+
+    @Test
+    void saveIOExceptionNonExistedDirectoryTest() throws Exception{
+        //Arrange
+        File folder = new File("Z:/nieistniejacykatalog");
+        Image img1 = new Image("src/test/resources/testowy/Archer.(Fate.stay.night).full.1801264.jpg");
+        List<Image> images = Arrays.asList(img1);
+
+        //Act & Assert
+        Exception exception = assertThrows(IOException.class, () -> FileManager.Save(folder, images));
+        assertEquals("Failed to create folder Z:\\nieistniejacykatalog\\2014\\11\\17", exception.getMessage());
+    }
+
+    @Test
+    void saveIOExceptionFileAsDirectoryTest() throws Exception{
+        //Arrange
+        Path file = Files.createTempFile("temp", ".jpg");
+        File folder = file.toFile();
+        Image img1 = new Image("src/test/resources/testowy/Archer.(Fate.stay.night).full.1801264.jpg");
+        List<Image> images = Arrays.asList(img1);
+
+        //Act & Assert
+        Exception exception = assertThrows(IOException.class, () -> FileManager.Save(folder, images));
+        assertTrue(exception.getMessage().contains("Failed to create folder"));
     }
 }
